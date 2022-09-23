1f80961a4919603cdb366182657378b923b965dc
Soumya Kundu <soumyakundu55@gmail.com>
2022-09-23 09:32:41 +0530

    added ctk codes from abhis pc
    1) Work next on using and changing between multiple windows

---
+++ cust.py
@@ -0,0 +1,39 @@
+import customtkinter as ctk
+import tkinter as ttk
+
+ctk.set_default_color_theme("dark-blue")  
+ctk.set_appearance_mode("dark")
+
+
+def ch_p1():
+    p1.pack(fill='both', expand=True)
+    p2.pack_forget()
+
+def ch_p2():
+    p2.pack(fill='both', expand=True)
+    p1.pack_forget()
+
+root = ctk.CTk()
+root.geometry('500x300')
+root.title('Python GUI')
+
+p1 = ctk.CTkFrame(master=root)
+
+# p1.grid_rowconfigure(0, weight=1)
+# p1.grid_columnconfigure(0, weight=1)
+
+p1.pack(fill="both", expand=True)
+label_1 = ctk.CTkLabel(master=p1, justify=ttk.LEFT, text='This is Page 1')
+label_1.pack(pady=12, padx=10)
+
+p2 = ctk.CTkFrame(master=root)
+label_2 = ctk.CTkLabel(master=p2, justify=ttk.LEFT, text='This is Page 2')
+label_2.pack(pady=12, padx=10)
+
+b1 = ctk.CTkButton(master=p2, text='Page 1', command=ch_p1)
+b1.pack(padx=20, pady=10)
+
+b2 = ctk.CTkButton(master=p1, text='Page 2', command=ch_p2)
+b2.pack(padx=20, pady=10)
+
+root.mainloop()
